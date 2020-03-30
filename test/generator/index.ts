8025b5a9f64cd7cfb4690c5b94dd7346482dbd5f
Frank Stokes <frank@wearereasonablepeople.com>
2020-03-30 15:20:04 +0200

    Add tests for internal registers/wires

--- test/generator/index.ts
+++ test/generator/index.ts
@@ -1,6 +1,8 @@
 import * as mocha from 'mocha';
 import headers from './headers';
+import internalRegisters from './internal-registers';
 
 describe('generator', () => {
   describe('headers', headers);
+  describe('internalRegisters', internalRegisters);
 })
